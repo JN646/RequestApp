06362473cf297795e03715a7ab6b4d5f89a043a4
Josh <josh.ginn@hotmail.co.uk>
2018-12-26 12:00:20 +0000

    Navbar align right. add conciege icon.

--- partials/_header.php
+++ partials/_header.php
@@ -46,12 +46,12 @@
   </head>
 
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="<?php echo $environment; ?>index.php">Request App</a>
+  <a class="navbar-brand" href="<?php echo $environment; ?>index.php"><i class="fas fa-concierge-bell"></i> RequestApp</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
+    <ul class="nav navbar-nav ml-auto w-100 justify-content-end">
       <li class="nav-item">
         <a class="nav-link" href="<?php echo $environment; ?>index.php"><i class='fas fa-home'></i></a>
       </li>
