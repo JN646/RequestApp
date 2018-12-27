0cad1c6e1d8e780b87b27305f0def256be195729
Josh <josh.ginn@hotmail.co.uk>
2018-12-27 16:29:27 +0000

    Session Tweaks.

--- lib/session_test.php
+++ lib/session_test.php
@@ -1,15 +1,20 @@
+<style media="screen">
+  body {
+    font-family: monospace;
+  }
+</style>
 <?php
 include 'sessions_class.php';
 include '../config/db_config.php';
 
 $table = '5';
 
-$session = new Session('1','1',$table);
+$session = new Session($table);
 
 $session->countSession($link);
 $session->countSessionClosed($link);
 $session->createSessionDB($link,$table);
-// $session->destroySessionSession();
+// $session->destroySessionSession($link);
 
 if (isset($_SESSION['session'])) {
   echo "Success: " . $_SESSION['session'];
--- lib/sessions_class.php
+++ lib/sessions_class.php
@@ -7,19 +7,36 @@ include '../config/db_config.php';
 class Session
 {
   private $sessionID;
-  private $sessionActive;
   private $sessionTable;
 
   // Constructor
-  function __construct($sessionID,$sessionActive,$sessionTable)
+  function __construct($sessionTable)
   {
-    $this->sessionID = $sessionID;
-    $this->sessionActive = $sessionActive;
     $this->sessionTable = $sessionTable;
   }
 
   // Destroy the Session.
-  function destroySessionSession() {
+  function destroySessionSession($link) {
+    $sqlCheck = "SELECT * FROM sessions
+    WHERE session_location_id = '$sessionTable'
+    AND session_closed = '0'";
+    $result = mysqli_query($link, $sqlCheck);
+
+    if (mysqli_num_rows($result) != 0) {
+      echo "There is already a session. </br>";
+    } else {
+
+      // Error catch
+      if (!$result) {
+          echo "New record created successfully </br>";
+          $_SESSION['session'] = $sessionTable;
+          echo "<p style='color: green;'>Session was set.</p>";
+      } else {
+          echo "Error: " . mysqli_error($link);
+      }
+    }
+
+    // Unset the session
     unset($_SESSION['session']);
 
     echo "<p style='color: red;'>Session was destroyed.</p>";
@@ -27,9 +44,8 @@ class Session
 
   // Count Number of Sessions
   function countSession($link) {
-    // Count Types
+    // Count Sessions
     $query = "SELECT COUNT(*) FROM sessions";
-
     $result = mysqli_query($link, $query);
     $rows = mysqli_fetch_row($result);
 
@@ -38,49 +54,41 @@ class Session
     if ($count == '') {
       $count = 'ERROR';
     }
-
-    // Return Value.
     echo "Number of Sessions: " . $count . "</br>";
   }
 
   // Add Session to database.
   function createSessionDB($link,$sessionTable) {
-    $sqlCheck = "SELECT * FROM sessions WHERE  session_location_id = '$sessionTable' AND session_closed = '0'";
+    $sqlCheck = "SELECT * FROM sessions
+    WHERE session_location_id = '$sessionTable'
+    AND session_closed = '0'";
     $result = mysqli_query($link, $sqlCheck);
 
     if (mysqli_num_rows($result) != 0) {
       echo "There is already a session. </br>";
     } else {
-      $sqlAdd = "INSERT INTO sessions (session_location_id) VALUES ($sessionTable)";
 
       // Error catch
-      if (mysqli_query($link, $sqlAdd)) {
-          echo "New record created successfully </br>";
-          $_SESSION['session'] = $sessionTable;
-          echo "<p style='color: green;'>Session was set.</p>";
-      } else {
-          echo "Error: " . $sqlAdd . "<br>" . mysqli_error($link);
+      if (!$result) {
+          echo "Error: " . mysqli_error($link) . " </br>";
       }
     }
-
-    mysqli_close($link);
   }
 
   // Count number of closed sessions.
   function countSessionClosed($link) {
     // Count Types
-    $query = "SELECT COUNT(*) FROM sessions WHERE session_closed = 1 AND session_location_id = '$sessionTable'";
-
+    $query = "SELECT COUNT(*) FROM sessions
+    WHERE session_closed = 1";
     $result = mysqli_query($link, $query);
     $rows = mysqli_fetch_row($result);
 
     $count = $rows[0];
 
-    if ($count == '') {
-      $count = 'ERROR';
+    // Error catch
+    if (!$result) {
+        echo "Error: " . mysqli_error($link) . " </br>";
     }
-
-    // Return Value.
     echo "Number of Sessions Closed: " . $count . "</br>";
   }
 }
