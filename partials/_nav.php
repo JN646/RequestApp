1f65b2cf33095df84118389868e59a175097a6b0
Josh <josh.ginn@hotmail.co.uk>
2018-12-29 17:57:27 +0000

    Simplify debug nav for smaller screens.

--- partials/_nav.php
+++ partials/_nav.php
@@ -7,22 +7,22 @@
     <a class='navbar-brand' href='#'>DEBUG</a>
     <ul class='nav'>
       <li class='nav-item'>
-        <a class='nav-link' href='<?php echo $environment; ?>crud/index.php'>Item</a>
+        <a class='nav-link' href='<?php echo $environment; ?>crud/index.php'>I</a>
       </li>
       <li class='nav-item'>
-        <a class='nav-link' href='<?php echo $environment; ?>crud/type_crud.php'>Type</a>
+        <a class='nav-link' href='<?php echo $environment; ?>crud/type_crud.php'>T</a>
       </li>
       <li class='nav-item'>
-        <a class='nav-link' href='<?php echo $environment; ?>crud/location_crud.php'>Locations</a>
+        <a class='nav-link' href='<?php echo $environment; ?>crud/location_crud.php'>L</a>
       </li>
       <li class='nav-item'>
-        <a class='nav-link' href='<?php echo $environment; ?>crud/sessions_crud.php'>Sessions</a>
+        <a class='nav-link' href='<?php echo $environment; ?>crud/sessions_crud.php'>S</a>
       </li>
       <li class='nav-item'>
-        <a class='nav-link' href='<?php echo $environment; ?>config/admin.php'><i class='fas fa-unlock-alt'></i></a>
+        <a class='nav-link disabled' disabled href='<?php echo $environment; ?>config/admin.php'><i class='fas fa-unlock-alt'></i></a>
       </li>
       <li class='nav-item'>
-        <a class='nav-link' href='<?php echo $environment; ?>request/request.php'>Request Home</a>
+        <a class='nav-link' href='<?php echo $environment; ?>request/request.php'>Req</a>
       </li>
     </ul>
   </nav>
